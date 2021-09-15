809f713f7ae465212f040906afb3450b5ec0d3b3
josecostadev <89701911+josecostadev@users.noreply.github.com>
2021-09-15 21:41:01 +0100

    Episode 014
    Vue.js

---
+++ client/src/views/Groups.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>HELLO FROM GROUPS</h1>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+    components: {
+
+    }
+})
+export default class Groups extends Vue {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
